b70b35c45920ac5d71ed01afb6cce85a3d19a9b8
prathmesh_gumal <prathmeshgumal@gmail.com>
2025-08-18 10:11:02 +0530

    .

--- game.py
+++
@@ -1,7 +0,0 @@
-import numpy as np 
-import pandas as pd
-a01,a02,a03,a04,a11,a12,a13,a14,a21,a22,a23,a24,a31,a32,a33,a34,a41,a42,a43,a44=0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-board = np.array([[a01,a02,a03,a04],[a11,a12,a13,a14],[a21,a22,a23,a24],[a31,a32,a33,a34],[a41,a42,a43,a44]])
-print(board)
-
-
